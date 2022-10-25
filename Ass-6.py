fdc7f8f837f089ad3ec064e8492f2c9986b77dc2
Rextin <fungtinrex04@gmail.com>
2022-10-25 22:53:24 +0800

    week-6 Ass6 25/10 commit

--- Ass-6.py
+++ Ass-6.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from numpy import record
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import time
+import MySQLdb.cursors          # import the db and use methods
+import time                     # for the timestamp to database
 import datetime
 
-ts = time.time()
-timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+ts = time.time() # def time :
+timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S') 
 
 app=Flask(__name__ )
 # Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key="test your page!"
 
 
-# Enter your database connection details below
+# Enter database connection details 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'website'
+app.config['MYSQL_DB'] = 'website' # use the database
 
-# Intial MySQL
+#  let = mysql , Intial MySQL (class)
 mysql = MySQL(app)
 
 
@@ -34,31 +34,31 @@ def register():
     username=request.form["username"]   # get the form name & password
     password=request.form["password"] 
 
-    cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM `member` WHERE `username` = %s ',(username,))
-    member=cursor.fetchone()
-    if member:
+    cursor=mysql.connection.cursor(MySQLdb.cursors.DictCursor) # use the method     
+    cursor.execute('SELECT * FROM `member` WHERE `username` = %s ',(username,)) #execute the select 
+    member=cursor.fetchone() # fetchone = get member table
+    if member: #if the member username exists 
         exists="Account already exists!"
         return redirect(url_for("error",exists=exists))
-    else:
+    else:                                               # the value column must same with database  
         cursor.execute('INSERT INTO member VALUES (NULL, %s, %s, %s,%s,%s)', (name,username, password,0,timestamp))
-        mysql.connection.commit()
-        return render_template("succeed.html",name=name)
+        mysql.connection.commit()  # push to database 
+        return render_template("succeed.html",name=name) # send to succeed page
 
 
 @app.route("/signin", methods=["POST"])
 def signin():
      
-    username=request.form["username"]   # get the form name & password
+    username=request.form["username"]   # get the form username & password
     password=request.form["password"] 
 
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute('SELECT * FROM `member` WHERE `username` = %s AND `password` = %s', (username, password,))
+    cursor.execute('SELECT * FROM `member` WHERE `username` = %s AND `password` = %s', (username, password,)) 
 # Fetch one record and return result
     member = cursor.fetchone()
     
-    if member:  # if  filter the name &password  
-        session['username'] = member['username']
+    if member:  # if  filter the username & password  
+        session['username'] = member['username'] # Create session data,can access this data in other routes
         session['password'] = member['password']
         return redirect(url_for("member"))
     elif username== "" or password == "":          
@@ -72,7 +72,7 @@ def signin():
 
 @app.route("/member")
 def member():
-    sId=session.get("username",None)
+    sId=session.get("username",None) # use session 
      #get method judge have session id or not 
     if not sId:
             return redirect(url_for("home"))
